61d612cdbcb96e71d3c8394f5640c02f42852d94
syedabdulbasit-ned <148267747+syedabdulbasit-ned@users.noreply.github.com>
2025-01-02 21:31:15 +0500

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+// Primitive Data Types
+let age = 25;          
+let name = "Alice";    
+let isActive = true;    
+let salary = null;     
+let city;               
+let bigNumber = 123n;   
+let uniqueId = Symbol("id");
+console.log(typeof age);      
+console.log(typeof name);     
+console.log(typeof isActive);  
+console.log(typeof salary);    
+console.log(typeof city);      
+console.log(typeof bigNumber); 
+console.log(typeof uniqueId);  
+
+let colors = ["red", "green", "blue"]; // Array
+let person = { name: "Alice", age: 25 }; // Object
+
+console.log(typeof colors); // "object"
+console.log(typeof person); // "object"
