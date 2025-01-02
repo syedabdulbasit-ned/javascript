e26ed2a102eeb1e288e400afcd171657a163699f
syedabdulbasit-ned <148267747+syedabdulbasit-ned@users.noreply.github.com>
2025-01-02 21:47:10 +0500

    while+do while

---
+++ while.js
@@ -0,0 +1,14 @@
+let count = 0;
+while (count < 3) {
+    console.log(`Count is: ${count}`);
+    count++;
+}
+
+
+//do while
+
+// let count = 0;
+// do {
+//     console.log(`Count is: ${count}`);
+//     count++;
+// } while (count < 3);
